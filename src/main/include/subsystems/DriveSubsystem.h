0c577e50aa7d1dd3d9c9c67502ae5d0e4c5e55a2
titansrobotics <titanrobotics8174@gmail.com>
2023-03-22 15:20:34 -0600

    changed stuff in DriveSubsystem.h

--- src/main/include/subsystems/DriveSubsystem.h
+++ src/main/include/subsystems/DriveSubsystem.h
@@ -1,6 +1,30 @@
 #ifndef DRIVE_SUBSYSTEM_H
 #define DRIVE_SUBSYSTEM_H
 
+#include <frc/drive/DifferentialDrive.h>
 #include <ctre/phoenix/motorcontrol/can/VictorSPX.h>
+#include <frc2/command/SubsystemBase.h>
+
+#include "Constants.h"
+
+
+class DriveSubystem : public frc2::SubsystemBase{
+    public:
+
+    DriveSubystem();
+
+    void Periodic() override;
+
+    void ArcadeDrive(double fwd, double rot);
+
+    void ResetEncoders();
+
+    double GetAverageEncoderDistance();
+
+    frc::Encoder& GetLeftEncoder();
+    frc::Encoder& GetRightEncoder();
+
+    void SetMaxOutput(double maxOutput);
+};
 
 #endif

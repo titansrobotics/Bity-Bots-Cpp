182576bb9276fba1444821d5bfa3d2d4cba75a38
Navynickel <navynickel30757@gmail.com>
2023-08-02 17:18:20 -0600

    idk what happened, some merge conflict

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,8 +1,6 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define M_PI 3.14159265358979323846
-
 namespace DriveConstants {
 
     constexpr int kLeftMotorId  {1};
@@ -11,17 +9,10 @@ namespace DriveConstants {
     constexpr int kLeftEncoderId[2]  {0, 1};
     constexpr int kRightEncoderId[2] {2, 3};
 
-<<<<<<< HEAD
     constexpr double kCountsPerRevolution {2048};
     constexpr double kWheelDiameter       {6};
     constexpr double kDistancePerPulse    {kWheelDiameter*std::numbers::pi/kCountsPerRevolution};
     //Assuming the encoder is mounted directly on the wheel shaft
-=======
-constexpr double countsPerRevolution {2048};
-constexpr double wheelDiameter       {6};
-constexpr double distancePerPulse    {wheelDiameter*M_PI/countsPerRevolution};
-//Assuming Encoder is on the shaft
->>>>>>> 6ed662358d71625d33495ffb93cd5a4fd67d4be0
 
 }
 
@@ -39,13 +30,8 @@ namespace ArmConstants {
 
     constexpr int kEncoderId[2] {4, 5};
 
-<<<<<<< HEAD
     constexpr double kCountsPerRevolution {1};
     constexpr double kAnglePerPulse       {2*std::numbers::pi/kCountsPerRevolution};
-=======
-constexpr double countsPerRevolution {1};
-constexpr double anglePerPulse       {2*M_PI/countsPerRevolution};
->>>>>>> 6ed662358d71625d33495ffb93cd5a4fd67d4be0
 
 }
 
@@ -60,7 +46,6 @@ namespace ClawConstants {
 
 namespace SpeedConstants {
 
-<<<<<<< HEAD
     constexpr int kDriveForwardPercentage  {0.6};
     constexpr int kDriveRotationPercentage {0.3};
 
@@ -69,16 +54,6 @@ namespace SpeedConstants {
     constexpr int kElevatorPercentage {0.3};
 
     constexpr int kClawPercentage {0.3};
-=======
-constexpr double driveForwardPercentage  {0.6};
-constexpr double driveRotationPercentage {0.3};
-
-constexpr double armPercentage {0.5};
-
-constexpr double elevatorPercentage {0.3};
-
-constexpr double clawPercentage {0.3};
->>>>>>> 6ed662358d71625d33495ffb93cd5a4fd67d4be0
 
 }
 
